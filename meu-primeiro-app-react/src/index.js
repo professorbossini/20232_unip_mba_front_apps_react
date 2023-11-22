29e6938bf9501dcd8bc9a68e739967e694c6ec24
Rodrigo Bossini <professorbossini@gmail.com>
2023-11-22 19:15:25 -0300

    front: working on the props app

---
+++ meu-primeiro-app-react/src/index.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './styles.css'
+
+const App = () => {
+  const estilosBotao = {marginTop: 12, paddingTop: 8, paddingBottom: 8, width: '100%', backgroundColor: 'blueviolet', color: 'white', border: 'none', borderRadius: 8}
+
+  const textoDoRotulo = 'Nome:'
+
+  const obterTextoDoBotao = () => {
+    return 'Enviar'
+  }
+  
+  //expressão JSX
+  return (
+    <div style={{margin: 'auto', width: 768, backgroundColor: '#EEE', padding: 12, borderRadius: 8}}>
+      <label className='rotulo' htmlFor='nome' style={{display: 'block', marginBottom: 4}}>
+        {textoDoRotulo}
+      </label>
+      <input type='text' id='nome' style={{paddingTop: 8, paddingBottom: 8, borderStyle: 'hidden', width: '100%', borderRadius: 8, outline: 'none'}} />
+      <button style={estilosBotao}>
+        {obterTextoDoBotao()}
+      </button>
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <App />,
+  document.querySelector('#root')  
+)
---
+++ react-componentes-props/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+  return (
+    <div>Um componente</div>
+  )
+}
+
+ReactDOM.render(
+  <App />,
+  document.querySelector('#root')
+)
