a62d7568123dd14d6ef4eb084d5171d64fd31de8
Rodrigo Bossini <professorbossini@gmail.com>
2023-12-06 22:10:57 -0300

    ap06: fim da secao 2.2

---
+++ exibe-fotos-pexels/src/index.js
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom'
+import App from './components/App'
+import 'primereact/resources/primereact.min.css'
+import 'primeicons/primeicons.css'
+import 'primeflex/primeflex.css'
+import 'primereact/resources/themes/tailwind-light/theme.css'
+ReactDOM.render(
+  <App />,
+  document.querySelector('#root')
+)
